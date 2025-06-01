5c8530549f45ac96efe6356c3e76d3b515141a54
Guzmán285 <herberthguzman0@gmail.com>
2025-06-01 02:53:45 -0600

    index

--- public/index.php
+++ public/index.php
@@ -1,13 +1,22 @@
 <?php 
 require_once __DIR__ . '/../includes/app.php';
 
-
 use MVC\Router;
 use Controllers\AppController;
+use Controllers\ProductosController;
+
 $router = new Router();
 $router->setBaseURL('/' . $_ENV['APP_NAME']);
 
+// Ruta principal
 $router->get('/', [AppController::class,'index']);
+
+// Rutas de productos
+$router->get('/productos', [ProductosController::class, 'renderizarPagina']);
+$router->get('/productos/buscarAPI', [ProductosController::class, 'buscarProductosAPI']);
+$router->post('/productos/guardarAPI', [ProductosController::class, 'guardarAPI']);
+$router->post('/productos/modificarAPI', [ProductosController::class, 'modificarAPI']);
+$router->get('/productos/eliminarAPI', [ProductosController::class, 'eliminarAPI']);
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
